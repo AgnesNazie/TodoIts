48e435558d3fef10b24a3db1966995f51db48a9f
AgnesNazie <cheagnesmbo@gmail.com>
2025-05-15 21:18:03 +0200

    created Exception package and custom exception

---
+++ src/main/java/se/Lexicon/Exception/AppUserNotFoundException.java
@@ -0,0 +1,11 @@
+package se.Lexicon.Exception;
+
+public class AppUserNotFoundException extends RuntimeException {
+    public AppUserNotFoundException(String message) {
+        super(message);
+    }
+
+    public AppUserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/se/Lexicon/Exception/UserNameAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package se.Lexicon.Exception;
+
+public class UserNameAlreadyExistsException extends RuntimeException {
+    public UserNameAlreadyExistsException(String message) {
+        super(message);
+    }
+
+    public UserNameAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
